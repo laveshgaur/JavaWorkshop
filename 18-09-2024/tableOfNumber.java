1a15091d5f31a3f3a5216efa983f129396d9245e
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 16:01:35 +0530

    Table Printer

---
+++ 18-09-2024/tableOfNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class tableOfNumber{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Table printer\nEnter a number : ");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d X %d = %d\n",n,i,n*i);
+        }
+    }
+}
