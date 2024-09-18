0c039d9fa55a85fdc415469d0b82336990054ab8
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 16:12:34 +0530

    Sum of first n multiples of 3

---
+++ 18-09-2024/sumOfMultipleOfThree.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sumOfMultipleOfThree{
+    public int sum(int n){
+        int ans=0;
+        for(int i=0,a=3;i<n;i++,a+=3){
+            ans+=a;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sum of first n multiples of 3\nEnter a number : ");
+        int n = sc.nextInt();
+        sumOfMultipleOfThree s = new sumOfMultipleOfThree();
+        int ans = s.sum(n);
+        System.out.println("Sum is : "+ans);
+    }
+}
