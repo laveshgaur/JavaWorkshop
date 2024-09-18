6700e4f2a198f8cf85ebeb8cbf9023549ae3cb00
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 16:08:27 +0530

    Sum of first n even numbers

--- 18-09-2024/sumOfEvenNatural.java
+++ 18-09-2024/sumOfEvenNatural.java
@@ -2,17 +2,17 @@ import java.util.*;
 class sumOfEvenNatural{
     public int sum(int n){
         int ans=0;
-        for(int i=2;i<=n;i+=2){
-            ans+=i;
+        for(int i=0,a=2;i<n;i++,a+=2){
+            ans+=a;
         }
         return ans;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Sum of even numbers upto n\nEnter a number : ");
+        System.out.print("Sum of n even numbers\nEnter a number : ");
         int n = sc.nextInt();
         sumOfEvenNatural s = new sumOfEvenNatural();
         int ans = s.sum(n);
-        System.out.println("Sum of even natural number is : "+ans);
+        System.out.println("Sum is : "+ans);
     }
 }
