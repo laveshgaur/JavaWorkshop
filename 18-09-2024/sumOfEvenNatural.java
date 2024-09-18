a23dd4993cdc8055ae7fc08155fbbcd5374964d1
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 15:45:18 +0530

    Sum of even numbers upto n

---
+++ 18-09-2024/sumOfEvenNatural.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sumOfEvenNatural{
+    public int sum(int n){
+        int ans=0;
+        for(int i=2;i<=n;i+=2){
+            ans+=i;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sum of even numbers upto n\nEnter a number : ");
+        int n = sc.nextInt();
+        sumOfEvenNatural s = new sumOfEvenNatural();
+        int ans = s.sum(n);
+        System.out.println("Sum of n natural number is : "+ans);
+    }
+}
