0c39a3fcbc9bcc49cc12326369a21c7b5762a6a1
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 12:18:55 +0530

    Fibonacci Series

---
+++ 18-09-2024/fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class fibonacci{
+    public int fibo(int n){
+        if (n<=1){
+            return n;
+        }
+        else{
+            return (fibo(n-1)+fibo(n-2));
+        }
+    }
+    public static void main(String args[]){
+        fibonacci f = new fibonacci();
+        System.out.print("Fibonacci series\nEnter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print(f.fibo(i)+" ");
+        }
+    }
+}
