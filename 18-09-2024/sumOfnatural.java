3ae86fc4c706e54e9a3274c9d3633e0195ed964a
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 15:38:49 +0530

    Sum of first n natural numbers

---
+++ 18-09-2024/sumOfnatural.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sumOfnatural{
+    public int sum(int n){
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+=i;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sum of n natural numbers\nEnter a number : ");
+        int n = sc.nextInt();
+        sumOfnatural s = new sumOfnatural();
+        int ans = s.sum(n);
+        System.out.println("Sum of n natural number is : "+ans);
+    }
+}
