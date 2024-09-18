90ae46c563a62df56e08793a58db8fe055b492d5
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-18 10:44:19 +0530

    Balance Check

---
+++ 18-09-2024/balance.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class balance{
+    public void checkBalance(Double amount){
+        if(amount>1000){
+            System.out.println("Balance is Sufficient");
+        }
+        else{
+            System.out.println("Balance is not Sufficient");
+        }
+    }
+    public static void main(String args[]){
+        balance b = new balance();
+        System.out.print("Enter your balance : ");
+        Scanner sc = new Scanner(System.in);
+        Double bal = sc.nextDouble();
+        b.checkBalance(bal);
+
+    }
+}
