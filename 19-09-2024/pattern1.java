fc02bb8034ed77a596caf9a0692b4b8eca9f5ca1
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-19 09:44:27 +0530

    Pattern 1

--- 19-09-2024/pattern1.java
+++ 19-09-2024/pattern1.java
@@ -2,6 +2,7 @@ import java.util.*;
 class pattern1{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=0;j<i;j++){
