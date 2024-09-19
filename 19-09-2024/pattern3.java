f7efdb6b9333d04001da23cdfc5f7f923ab463ae
Lavesh Gaur <162182397+laveshgaur@users.noreply.github.com>
2024-09-19 10:01:17 +0530

    Pattern 3

---
+++ 19-09-2024/pattern3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class pattern3{
+    public static void main(String args[]){
+        for(int i=5;i<=25;i+=5){
+            for(int j=25;j>=i;j-=5){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
